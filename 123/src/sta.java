cab17da9c0492c2cc18426f95f0e95d60086c9dd
MarkQinsama <810019102@qq.com>
2019-10-18 11:19:38 +0800

    1231111

---
+++ 123/src/sta.java
@@ -0,0 +1,11 @@
+public class sta {
+    public static void main(String[] args) {
+        System.out.println(13123123);
+        System.out.println(13123123);
+        System.out.println(13123123);
+        System.out.println(13123123);
+        System.out.println(13123123);
+        System.out.println(13123123);
+        System.out.println(13123123);
+    }
+}
